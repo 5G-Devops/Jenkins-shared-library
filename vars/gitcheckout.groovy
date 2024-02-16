66512d479e0957b2d88d95964e21ad8918fff592
Gowtham <venkatgowtham911@gmail.com>
2024-02-16 23:07:20 +0530

    commit

--- vars/gitcheckout.groovy
+++ vars/gitcheckout.groovy
@@ -1,8 +1,8 @@
 
 def call(MAP stageparams){
     checkout scmGit(
-        $class: 'GitSCM'
-         branches: [[name:stageparams.branch ]],
+         $class: 'GitSCM'
+         branches: [[name: stageparams.branch ]],
          userRemoteConfigs: [[url: stageparams.url]]
          )
 
