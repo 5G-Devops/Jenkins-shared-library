3ee3660b3a2583baea1f5ba1160eaa858e9c24e5
Gowtham <venkatgowtham911@gmail.com>
2024-02-18 18:08:50 +0530

    imagebuild

---
+++ vars/dockerbuild.groovy
@@ -0,0 +1,8 @@
+def call(){
+    sh """
+     docker build -t ${userhub}/${imagename} .
+     docker image tag ${userhub}/${imagename} ${userhub}/${imagename}:${imagetag}
+     docker image tag ${userhub}/${imagename}:latest
+
+    """
+}
