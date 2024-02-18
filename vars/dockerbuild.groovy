c60c6505a39acc70c9d1c825bfb1f9dd2c1a8afc
Gowtham <venkatgowtham911@gmail.com>
2024-02-18 18:33:58 +0530

    imagebuild

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -2,7 +2,6 @@ def call(){
     sh """
      docker build -t radhagowthamhub/javaapp .
      docker image tag radhagowthamhub/javaapp radhagowthamhub/javaapp:v1
-     docker image tag radhagowthamhub/javaapp:latest
 
     """
 }
