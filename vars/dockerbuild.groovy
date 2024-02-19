67c3c3459ac0628bd326ed0dcd47b1a0ca7066ad
Gowtham <venkatgowtham911@gmail.com>
2024-02-19 19:12:38 +0530

    commit

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,7 +1,7 @@
 def call(String userHub, String imageNme, String imageTag ){
     sh """
-     docker build -t ${userHub}${imageNme}.
-     docker image tag ${userHub}${imageNme} ${userHub}${imageNme}:${imageTag}
-     docker image tag ${userHub}${imageNme} ${userHub}${imageNme}:latest
+     docker build -t ${userHub}/${imageNme}.
+     docker image tag ${userHub}/${imageNme} ${userHub}/${imageNme}:${imageTag}
+     docker image tag ${userHub}/${imageNme} ${userHub}/${imageNme}:latest
     """
 }
