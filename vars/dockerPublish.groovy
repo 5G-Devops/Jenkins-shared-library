75f26deaa011689766e10dfa4b6f64e80f33906c
Gowtham <venkatgowtham911@gmail.com>
2024-02-19 12:30:53 +0530

    commit

---
+++ vars/dockerPublish.groovy
@@ -0,0 +1,11 @@
+def call(){
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerloginid', 
+        passwordVariable: 'PASS', 
+        usernameVariable: 'USER'
+        )]) {
+    sh 'docker login -u '$USER' -p '$PASS''
+    sh 'docker image push radhagowthamhub/javaapp:v1'
+    sh 'docker image push radhagowthamhub/javaapp:latest'
+}
+}
