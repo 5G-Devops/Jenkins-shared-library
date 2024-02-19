f2591881f56d68ef485dad7ec4358c4a5e70b94b
Gowtham <venkatgowtham911@gmail.com>
2024-02-19 12:08:31 +0530

    commit

--- vars/dockerImgscan.groovy
+++ vars/dockerImgscan.groovy
@@ -1,6 +1,6 @@
 def call(){
     sh """
-     trivy image radhagowthamhub/javaapp radhagowthamhub/javaapp:latest > scan.txt
+     trivy image radhagowthamhub/javaapp:latest > scan.txt
      cat scan.txt
     """
 }
